fd30121e7370182cdce896c151d91051c1df3dc7
jfernandes2208 <63377158+jfernandes2208@users.noreply.github.com>
2020-05-12 17:09:12 +0100

    Add files via upload

---
+++ build/precache-manifest.210c3a70d804b3feaf7c34af66f9e86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82f4046fbd0bcfac7788b06401525ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81732b522c65a3cd6ef",
+    "url": "/static/css/main.87126acd.chunk.css"
+  },
+  {
+    "revision": "38f198384117d84a806f",
+    "url": "/static/js/2.c99d77df.chunk.js"
+  },
+  {
+    "revision": "4901ab810476dcc34275748412cf5f2f",
+    "url": "/static/js/2.c99d77df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81732b522c65a3cd6ef",
+    "url": "/static/js/main.6268e673.chunk.js"
+  },
+  {
+    "revision": "f0af1da74e458ca2c809",
+    "url": "/static/js/runtime-main.157fafca.js"
+  }
+]);
