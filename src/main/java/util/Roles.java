c29c23f2afe90fa8b4061ccbf18f2e6f38c4186f
Casquilho <jm.casquilho@campus.fct.unl.pt>
2020-05-28 17:39:43 +0100

    b4 objectify

--- src/main/java/util/Roles.java
+++ src/main/java/util/Roles.java
@@ -3,7 +3,15 @@ package util;
 import java.util.List;
 import java.util.Map;
 
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+
 public class Roles {
+	private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+	private final KeyFactory roleKeyFactory = datastore.newKeyFactory().setKind("Role");
 	
 	public Map<String, List<String>> rolesTable;
 	
@@ -12,6 +20,10 @@ public class Roles {
 	
 	public Roles(Map<String, List<String>> rolesTable) {
 		this.rolesTable = rolesTable;
+	}
+	
+	public void getTable() {
+		
 	}
 
 }
